8970091d1183716a8b025a98e05ece3023bbd5cb
Ayush More <153529164+aayuuuush@users.noreply.github.com>
2025-03-21 19:20:01 +0000

    database modified to atlas

--- server/db.js
+++ server/db.js
@@ -3,16 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const atlasUri = 'mongodb+srv://techspace:9011232879@cluster0.31ygm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('MongoDB Connection Error:', error);
-    process.exit(1);
+      await mongoose.connect(atlasUri, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+      });
+      console.log('MongoDB Atlas connected');
+  } catch (err) {
+      console.error('MongoDB Atlas connection error:', err);
   }
 };
 

ddbc9092df02063c77a8d76cb091356b4f4a9eee
Ayush More <153529164+aayuuuush@users.noreply.github.com>
2025-03-27 22:00:24 +0530

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('http://localhost:5000/api/auth/login', {
+        const response = await fetch('https://comsy-sigma.vercel.app/api/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
